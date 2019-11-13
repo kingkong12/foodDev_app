eea7e29b9ecb0629028f0499b4edf2eaa6026778
vasanimithil999@gmail.com <vasanimithil999@gmail.com>
2019-11-13 10:03:33 -0500

    integrated navigation

---
+++ App/routes.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import {createAppContainer} from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text>Home!</Text>
+      </View>
+    );
+  }
+}
+
+class SettingsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text>Settings!</Text>
+      </View>
+    );
+  }
+}
+
+const TabNavigator = createBottomTabNavigator({
+  Menu: {screen: HomeScreen},
+  Orders: {screen: SettingsScreen},
+  Profile: {screen: HomeScreen},
+});
+
+export default createAppContainer(TabNavigator);
--- index.js
+++ index.js
@@ -3,7 +3,8 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import TabNavigator from './App/routes';
+//import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => TabNavigator);

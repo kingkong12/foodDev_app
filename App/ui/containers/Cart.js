613fc47f83c88f602b37ea50366d2e340f65bcc8
vasanimithil999@gmail.com <vasanimithil999@gmail.com>
2019-11-21 00:49:38 -0500

    modularizing cart view

--- App/ui/containers/Cart.js
+++ App/ui/containers/Cart.js
@@ -1,35 +1,8 @@
 import React from 'react';
 import {Text, StyleSheet, View, FlatList, Button, Image} from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import CartCard from '../atoms/CartCard';
 
 class Cart extends React.Component {
-  cartCard = () => {
-    return (
-      <View style={styles.cartCard}>
-        <View style={styles.imageContainer}>
-          <Image
-            onPress={() => {}}
-            source={{
-              uri:
-                'https://greatist.com/sites/default/files/lunch-wraps-feature-1800x1236.jpg',
-            }}
-            style={styles.image}
-          />
-        </View>
-        <View style={styles.descriptionContainer}>
-          <View style={styles.deatils}>
-            <Text style={styles.itemNameText}>WRAP</Text>
-            <Text style={styles.itemQuantityText}> Quantity: 2 </Text>
-          </View>
-          <View style={styles.editDetails}>
-            <Text style={styles.itemPrice}>$5.99 </Text>
-            <Text style={styles.editPrice}>Edit</Text>
-          </View>
-        </View>
-      </View>
-    );
-  };
-
   render() {
     const mockarry = [1, 2, 3];
     return (
@@ -41,7 +14,7 @@ class Cart extends React.Component {
           <FlatList
             data={mockarry}
             ItemSeparatorComponent={() => <View style={{height: 15}} />}
-            renderItem={({item}) => this.cartCard()}
+            renderItem={({item}) => <CartCard />}
             keyExtractor={(item, index) => `${index}`}
           />
         </View>
@@ -69,59 +42,6 @@ const styles = StyleSheet.create({
     color: 'tomato',
     fontSize: 24,
     fontWeight: 'bold',
-    textAlign: 'center',
-  },
-  cartCard: {
-    flex: 1,
-    width: '100%',
-    flexDirection: 'row',
-    height: '35%',
-    borderWidth: 1,
-    borderRadius: 10,
-    borderColor: 'tomato',
-  },
-  imageContainer: {
-    flex: 1,
-  },
-  image: {
-    width: '100%',
-    height: '100%',
-    borderRadius: 10,
-    minHeight: 70,
-  },
-  descriptionContainer: {
-    flex: 2,
-    flexDirection: 'row',
-  },
-  deatils: {
-    flex: 1,
-    color: 'tomato',
-    paddingTop: 12,
-    paddingLeft: 12,
-  },
-  editDetails: {
-    flex: 1,
-    paddingTop: 12,
-  },
-  itemNameText: {
-    color: 'tomato',
-    flex: 1,
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
-  itemQuantityText: {
-    flex: 1,
-    opacity: 0.5,
-  },
-  itemPrice: {
-    flex: 1,
-    color: '#85BB65',
-    fontSize: 18,
-    textAlign: 'center',
-  },
-  editPrice: {
-    flex: 1,
-    color: 'red',
     textAlign: 'center',
   },
   totalText: {

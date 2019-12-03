e8ec7deae357e21504c87ac187dc6a8538538b33
vasanimithil999@gmail.com <vasanimithil999@gmail.com>
2019-12-03 14:34:37 -0500

    implements edit feature

--- App/Actions/dashbaord.action.js
+++ App/Actions/dashbaord.action.js
@@ -4,3 +4,16 @@ export const addtoCartAction = item => {
     payload: item,
   };
 };
+
+export const deletefromCart = item => {
+  return {
+    type: 'DELETE_ITEM_FROM_CART',
+    payload: item,
+  };
+};
+export const udpateCartQuantity = item => {
+  return {
+    type: 'UPDATE_QUANTITY_FROM_CART',
+    payload: {id: item.id, quantity: item.quantity},
+  };
+};

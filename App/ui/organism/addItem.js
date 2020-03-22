c2b652648a4c6b39e298e722084078a0289ce8b6
vasanimithil999@gmail.com <vasanimithil999@gmail.com>
2020-03-22 11:04:19 +0530

    wiring up form addItemForm

--- App/ui/organism/addItem.js
+++ App/ui/organism/addItem.js
@@ -1,22 +1,104 @@
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
-import {TextInput, Headline} from 'react-native-paper';
+import {TextInput, Button, Headline} from 'react-native-paper';
+import ReactChipsInput from 'react-native-chips';
 
 class AddINewtem extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      itemName: '',
+      imageUrl: '',
+      pirce: '',
+      description: '',
+      ingredients: [],
+    };
+  }
+
+  onSubmitfun = () => {
+    let {itemName, imageUrl, pirce, description, ingredients} = this.state;
+    if (
+      itemName === '' ||
+      imageUrl === '' ||
+      pirce === '' ||
+      description === ''
+    ) {
+      //alert('cannot be empty');
+    }
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Headline style={styles.headingStyle}> Add item to list</Headline>
-        <View style={styles.rowCss}>
-          <TextInput dense label="Price" value={''} onChangeText={text => {}} />
-        </View>
-        <View style={styles.rowCss}>
-          <TextInput
-            multiline
-            label="Description"
-            value={''}
-            onChangeText={text => {}}
-          />
+        <View>
+          <Headline style={styles.headingStyle}> Add item to list</Headline>
+          <View style={styles.rowCss}>
+            <TextInput
+              dense
+              label="Name"
+              value={this.state.itemName}
+              onChangeText={text => this.setState({itemName: text})}
+            />
+          </View>
+          <View style={styles.rowCss}>
+            <TextInput
+              dense
+              label="Image URL"
+              value={this.state.imageUrl}
+              onChangeText={text => this.setState({imageUrl: text})}
+            />
+          </View>
+          <View style={styles.rowCss}>
+            <TextInput
+              dense
+              label="Price"
+              value={this.state.price}
+              onChangeText={text => this.setState({price: text})}
+            />
+          </View>
+          <View style={styles.rowCss}>
+            <TextInput
+              multiline
+              label="Description"
+              value={this.state.description}
+              onChangeText={text => this.setState({description: text})}
+            />
+            <View style={styles.nutrientsContainer}>
+              <ReactChipsInput
+                label="Add nutrients lables"
+                initialChips={this.state.ingredients}
+                onChangeChips={chips => this.setState({ingredients: chips})}
+                chipStyle={{
+                  borderColor: 'white',
+                  backgroundColor: 'tomato',
+                }}
+              />
+            </View>
+          </View>
+          <View style={styles.buttonStyle}>
+            <Button
+              color={'tomato'}
+              style={{width: '50%', marginRight: 2}}
+              mode="contained"
+              onPress={() => {
+                this.setState({
+                  itemName: '',
+                  imageUrl: '',
+                  pirce: '',
+                  description: '',
+                  ingredients: [],
+                });
+              }}>
+              Cancel
+            </Button>
+            <Button
+              color={'tomato'}
+              style={{width: '50%', marginRight: 2}}
+              mode="contained"
+              onPress={() => this.onSubmitfun()}>
+              Add Item
+            </Button>
+          </View>
         </View>
       </View>
     );
@@ -39,6 +121,13 @@ const styles = StyleSheet.create({
   rowCss: {
     paddingBottom: 16,
     marginHorizontal: 8,
+  },
+  buttonStyle: {
+    flexDirection: 'row',
+    marginTop: 16,
+  },
+  nutrientsContainer: {
+    minHeight: '30%',
   },
 });
 
